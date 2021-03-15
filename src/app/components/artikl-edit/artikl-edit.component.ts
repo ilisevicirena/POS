3c4494721aec4fd3cd167d240efa06f0f88f715b
irenailisevic <iilisevic@foi.hr>
2021-03-15 19:30:44 +0100

    Uredi artikl
    Artikl-edit component i template

--- src/app/components/artikl-edit/artikl-edit.component.ts
+++ src/app/components/artikl-edit/artikl-edit.component.ts
@@ -1,3 +1,4 @@
+import { browser } from 'protractor';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -5,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { param } from 'jquery';
 
+declare var $: any;
 
 @Component({
     selector:'artikl-edit',
@@ -20,6 +22,7 @@ export class ArtiklEditComponent implements OnInit {
     jed_mjere=" ";
     porez=0;
     cijena=0;
+    uspjesno=true;
     constructor(private route: ActivatedRoute, private http: HttpClient){
     
     }
@@ -40,7 +43,61 @@ export class ArtiklEditComponent implements OnInit {
     }
 
     spremiPromjene(){
-        console.log("novi broj"+this.broj);
+        this.http.put("http://localhost:8181/ords/in2/api/artikli",
+        {        
+        "id":  this.idArtikla,       
+        "naziv":  this.naziv,       
+        "broj": this.broj,
+        "jed_mjere": this.jed_mjere,
+        "porez" : this.porez,
+        "cijena" : this.cijena      
+        })    
+        .subscribe(
+     
+        data  => {       
+        this.uspjesno=true;
+        this.prikaziObavijest();
+        },
+        
+        error  => {
+        
+        this.uspjesno=false;
+        
+        }
+        
+        );
+    }
+
+    prikaziObavijest(){
+        const type = ['','info','success','warning','danger'];
+        let color = 2;
+        if(!this.uspjesno){
+            color=3;
+        }
+      
+        console.log("boja: "+color);
+      $.notify({
+          icon: "notifications",
+          message: "Promjene uspješno spremljene."
+
+      },{
+          type: type[color],
+          timer: 4000,
+          placement: {
+              from: 'top',
+              align: 'right'
+          },
+          template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+            '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+            '<i class="material-icons" data-notify="icon">notifications</i> ' +
+            '<span data-notify="title">{1}</span> ' +
+            '<span data-notify="message">{2}</span>' +
+            '<div class="progress" data-notify="progressbar">' +
+              '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            '</div>' +
+            '<a href="{3}" target="{4}" data-notify="url"></a>' +
+          '</div>'
+      });
     }
     
 
